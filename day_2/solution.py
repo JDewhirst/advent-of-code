5463c7f6d911cac1475bcd41cb730b25beda8474
Jack Dewhirst <jackdewhirst@gmail.com>
2020-12-02 16:01:11 +0000

    part 2

--- day_2/solution.py
+++ day_2/solution.py
@@ -21,3 +21,21 @@ def ValidPasswords(filename):
             num_valid += 1
             
     return num_valid
+    
+def RealValidPasswords(filename):
+    f = open(filename)
+    data = f.readlines()
+    f.close()
+    data = [item.strip("\n").split() for item in data]
+    
+    num_valid = 0
+    for entry in data:
+        pos1, pos2 = entry[0].split("-")
+        given_letter = entry[1][0]
+        password = entry[2]
+        
+        if (password[int(pos1)-1] == given_letter and password[int(pos2)-1] != given_letter) or (password[int(pos1)-1] != given_letter and password[int(pos2)-1] == given_letter):
+            num_valid += 1
+            
+    return num_valid
+        
