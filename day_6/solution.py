f84b337ddd35f2b48cd5954d1584e205277c0aee
Jack Dewhirst <jackdewhirst@gmail.com>
2020-12-06 20:05:17 +0000

    day 6 part 1

---
+++ day_6/solution.py
@@ -0,0 +1,19 @@
+import re
+
+def ReadDocuments(filename):
+    with open(filename,"r") as file:
+        data = [re.sub('\n',' ', line) for line in file.read().split("\n\n")]
+    return data
+    
+def SumGroupUniqueAnswers(data):
+    num_unique_answers = 0
+    for group in data:
+        group_set = set()
+        for character in group:
+            if character == " ":
+                pass
+            elif character not in group_set:
+                group_set.add(character)
+        num_unique_answers += len(group_set)
+    return num_unique_answers
+   
