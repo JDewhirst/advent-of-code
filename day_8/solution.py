a7c5b560fd9f8367a2735c4d8cc71bd8280e439a
Jack Dewhirst <jackdewhirst@gmail.com>
2020-12-08 21:56:52 +0000

    day 8 part 2

--- day_8/solution.py
+++ day_8/solution.py
@@ -19,16 +19,42 @@ def ExecCommand(data,i,acc):
     #print(f"i={i} acc={acc}")
     return i, acc
     
-if __name__=="__main__":
-    data = ReadDocuments("input.txt")
+def FindLoop(data):
     acc = 0
     i = 0
     visited_commands = set()
-    while i < len(data)-1:
-        #print(f"i={i} set={visited_commands}")
+    while i < len(data):
         if i in visited_commands:
-            print(f"Part 1: {acc}")
-            break
+            return acc, True
         visited_commands.add(i)
         i, acc = ExecCommand(data,i,acc)
+    return acc, False
+    
+def FixLoop(data):
+    i = 0
+    while i < len(data)-1:
+        data[i][0] = Flip(data[i][0])
+        acc, result = FindLoop(data)
+        #print(i, data)
+        if result == False:
+            return acc, i, data
+        data[i][0] = Flip(data[i][0])
+        i += 1
+        
+    return acc, i, data
+    
+def Flip(command):
+    if command == "nop":
+        command = "jmp"
+    elif command == "jmp":
+        command = "nop"
+    return command
+    
+if __name__=="__main__":
+    data = ReadDocuments("input.txt")
+    acc, Loop = FindLoop(data)
+    print(f"Part 1: acc = {acc} Loop = {Loop}")
+    if Loop:
+        acc2, i, data = FixLoop(data)
+        print(f"Part 2: acc = {acc2} Altered Line = {i}")
         
