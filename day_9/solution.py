8f6ae3df387079683c9cd77b94722a871c3ac815
Jack Dewhirst <jackdewhirst@gmail.com>
2020-12-09 15:12:49 +0000

    cleaned up loop with itertools

--- day_9/solution.py
+++ day_9/solution.py
@@ -1,28 +1,35 @@
+import itertools
+
 def ReadDocuments(filename):
     with open(filename,"r") as file:
         data = [int(line.strip("\n")) for line in file.readlines()]
     return data
     
-def FindWeakness(data,preamble_length):
+def FindInvalid(data,preamble_length):
     i = preamble_length
     while i < len(data):
+        a = [x for x in range(i-preamble_length,i)]
         is_sum = False 
-        for j in range(i-preamble_length,i):
-            for k in range(j+1,i):
-                #print(f"i={i}={data[i]} j={j}={data[j]} k={k}={data[k]} sum j+k ={data[j]+data[k]}" )
+        for j, k in itertools.combinations_with_replacement(a,2):
+            if j != k and (data[j]+data[k] == data[i]):
+            #print(f"i={i}={data[i]} j={j}={data[j]} k={k}={data[k]} sum j+k ={data[j]+data[k]}" )
                 if data[j]+data[k] == data[i]:
                     is_sum = True
-                    
+                if is_sum: break
+        
         if not is_sum:
             return i, data[i]
         else:
             i += 1
+            
 
+    
 
 if __name__=="__main__":
     # example.txt has a length 5 preamble and considers the 5 previous numbers
     # input.tt has a length 25 preamble and considers the 25 previous numbers
     data = ReadDocuments("input.txt")
-    line, value = FindWeakness(data,25)
-    print(value)
+    line, value = FindInvalid(data,25)
+    print(f"First invalid value = {value}")
+    
         
