bf81c72f978b1648176aed32e26e5f114c732be5
Jack Dewhirst <jackdewhirst@gmail.com>
2020-12-18 12:44:20 +0000

    day 18 part 1, smart solution with regex

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -1,6 +1,13 @@
 import re
 ### Calculator
 
+def Read(filename):
+    with open(filename,"r") as file:
+        data = file.readlines()
+    return [line.strip() for line in data]
+
+### Part 1
+
 ## Expressions
 # + addition
 # * multiplication
@@ -14,12 +21,8 @@ import re
 ## Examples
 # 1 + 2 * 3 = 9
 # 1 + (2 * 3) = 7
-
-def Read(filename):
-    with open(filename,"r") as file:
-        data = file.readlines()
-    return [line.strip() for line in data]
    
+# dumb solution
 num =  re.compile("[0-9]")
 
 def Operation(result, number, operator):
@@ -31,7 +34,7 @@ def Operation(result, number, operator):
         result += number
     return result
    
-def Evaluate(line, position):
+def Part1Eval(line, position):
     # iterate through string
     result = None
     operator = None
@@ -61,14 +64,45 @@ def Evaluate(line, position):
             return i+1, result
             
     return result
+    
+
+# part 1 smart solution with regex
+parens = re.compile(r'\(\s*(\d+)\s*\)')
+operation = re.compile(r'(\d+)\s*([+*])\s+(\d+)')
+
+def Eval(line):
+    while True:
+        line = re.sub(parens, r'\1', line)
+        match = re.search(operation, line)
+        if match:
+            lhs, op, rhs = match.groups()
+            if op == "+":
+                value = int(lhs) + int(rhs)
+            elif op == "*":
+                value = int(lhs) * int(rhs)
+            line = line[:match.start()] + str(value) + line[match.end():]
+        else:
+            break
+    return int(line)
+
+### Part 2
+# Same rules as prior except that addition has precedence over
+# multiplication
+# def Part2Eval(line, position):
+    
+    
+    
+    # # So I believe that the sensible way to do this is to atomise
+    # # if we have 1+2*3+4*5+6 we want to parse it like sort
+    # # (1+2)*(3+4)*(5+6) 
+    # # How do you do that ?
+    # # Well. Regex ?
+    # return False
         
     
 if __name__=="__main__":
     filename = "input.txt"
     data = Read(filename)
-    part_1_result = 0
-    for line in data:
-        #print(line)
-        part_1_result += Evaluate(line,0)
+    part_1_result = sum(map(Eval,data))
         
     print(f"Part 1: Sum of all expressions = {part_1_result}")
